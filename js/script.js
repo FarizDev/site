791b607703c86e6ac5574be96d2864496e33c08f
FarizDev <90859053+FarizDev@users.noreply.github.com>
2021-09-17 16:04:36 +0300

    new site

---
+++ js/script.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){
+
+    $('.slider').slick(
+        {
+            slidesToShow: 1,
+            responsive: 
+            [
+                {
+                  breakpoint: 650,
+                  settings: {
+                    autoplay: true,
+                    arrows: false
+                  }
+                },
+                
+            ]
+        }
+    );
+
+})
+
+
